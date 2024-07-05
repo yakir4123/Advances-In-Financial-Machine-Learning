c690a4385468240d14c03ce69701ac943cce0fd9
Yakir <yakir4123@gmail.com>
2024-07-05 18:37:17 +0300

    not using jesse for data read etc at the moment

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+import gc
+
 import numpy as np
+import pandas as pd
 from numba import njit
 
 
@@ -64,7 +67,10 @@ def imbalance_dollar_bar(prices: np.ndarray, qty: np.ndarray) -> np.ndarray:
 
 
 @njit
-def getTEvents(timestamps: np.ndarray, yt: np.ndarray, h: float) -> np.ndarray:
+def get_t_events(timestamps: np.ndarray, yt: np.ndarray, h: float) -> np.ndarray:
+    """
+    SNIPPET 2.4 THE SYMMETRIC CUSUM FILTER
+    """
     t_events = np.empty(len(timestamps), dtype=timestamps.dtype)
     s_pos = 0.0
     s_neg = 0.0
@@ -83,3 +89,65 @@ def getTEvents(timestamps: np.ndarray, yt: np.ndarray, h: float) -> np.ndarray:
             t_events[event_count] = timestamps[t + 1]
             event_count += 1
     return t_events[:event_count]
+
+
+# the data is downloaded im using is crypto ticker data from
+# https://www.binance.com/en/landing/data
+# using BTC for this exercise download data of 5 months
+def load_df(file: str) -> pd.DataFrame:
+    df = pd.read_csv(file)
+    del df["id"]
+    del df["quote_qty"]
+    del df["is_buyer_maker"]
+    gc.collect()
+    return reduce_memory_usage(df)
+
+
+def reduce_memory_usage(df: pd.DataFrame) -> pd.DataFrame:
+    start_mem = df.memory_usage().sum() / 1024**2
+    print(f"Memory usage of dataframe is {start_mem:.2f} MB")
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+
+            if pd.api.types.is_float_dtype(col_type):
+                df[col] = pd.to_numeric(df[col], downcast="float")
+            elif pd.api.types.is_integer_dtype(col_type):
+                if c_min >= 0:
+                    if c_max < 255:
+                        df[col] = df[col].astype(np.uint8)
+                    elif c_max < 65535:
+                        df[col] = df[col].astype(np.uint16)
+                    elif c_max < 4294967295:
+                        df[col] = df[col].astype(np.uint32)
+                    else:
+                        df[col] = df[col].astype(np.uint64)
+                else:
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif (
+                        c_min > np.iinfo(np.int16).min
+                        and c_max < np.iinfo(np.int16).max
+                    ):
+                        df[col] = df[col].astype(np.int16)
+                    elif (
+                        c_min > np.iinfo(np.int32).min
+                        and c_max < np.iinfo(np.int32).max
+                    ):
+                        df[col] = df[col].astype(np.int32)
+                    else:
+                        df[col] = df[col].astype(np.int64)
+        else:
+            num_unique_values = len(df[col].unique())
+            num_total_values = len(df[col])
+            if num_unique_values / num_total_values < 0.5:
+                df[col] = df[col].astype("category")
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print(f"Memory usage after optimization is: {end_mem:.2f} MB")
+    print(f"Decreased by {(start_mem - end_mem) / start_mem * 100:.1f}%")
+    return df
