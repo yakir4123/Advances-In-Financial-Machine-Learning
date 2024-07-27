d705a3239468e02627d00cdbf06dfbc5cc7c8cf7
Yakir <yakir4123@gmail.com>
2024-07-27 22:31:05 +0300

    wip chapter 5

--- utils/sample_weights.py
+++ utils/sample_weights.py
@@ -62,3 +62,49 @@ def exponential_time_decay(tw, exponent=2, clf_last_w=1.):
     clf_w /= clf_w.max()
     clf_w = np.power(clf_w, exponent) / exponent * clf_last_w
     return clf_w
+
+
+def get_ind_matrix(bars_indices: pd.Series, events: pd.Series) -> pd.DataFrame:
+    """
+    Snippet 4.3 derives an indicator matrix from two arguments: the index of bars
+    (bars_indices), and the pandas Series t1, which we used multiple times in Chapter 3. As
+    a reminder, t1 is defined by an index containing the time at which the features are
+    observed, and a values array containing the time at which the label is determined. The
+    output of this function is a binary matrix indicating what (price) bars influence the
+    label for each observation.
+    """
+    ind_m = pd.DataFrame(0, index=bars_indices, columns=range(events.shape[0]))
+    for i, (t0, t1) in enumerate(events.items()):
+        ind_m.loc[t0:t1, i] = 1.
+    return ind_m
+
+
+def get_avg_uniqueness(indM: pd.DataFrame) -> pd.Series:
+    """
+    Snippet 4.4 returns the average uniqueness of each observed feature. The input is
+    the indicator matrix built by getIndMatrix
+    """
+    # Average uniqueness from indicator matrix
+    c = indM.sum(axis=1)  # concurrency
+    u = indM.div(c, axis=0)  # uniqueness
+    avg_u = u[u > 0].mean()  # average uniqueness
+    return avg_u
+
+
+def seqBootstrap(ind_m: pd.DataFrame, s_length=None):
+    """
+    Snippet 4.5 gives us the index of the features sampled by sequential bootstrap. The
+    inputs are the indicator matrix (indM) and an optional sample length (sLength), with
+    a default value of as many draws as rows in indM
+    """
+    if s_length is None:
+        s_length = ind_m.shape[1]
+    phi = []
+    while len(phi) < s_length:
+        avg_u = pd.Series()
+        for i in ind_m:
+            ind_m_ = ind_m[phi + [i]]  # reduce indM
+            avg_u.loc[i] = get_avg_uniqueness(ind_m_).iloc[-1]
+        prob = avg_u / avg_u.sum()  # draw prob
+        phi += [np.random.choice(ind_m.columns, p=prob)]
+    return phi
